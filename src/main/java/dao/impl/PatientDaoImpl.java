014671d12bb680bcba3c44477136fbaaf371a2c9
ndjek1 <vndjekornom@gmail.com>
2025-06-29 19:54:20 +0300

    Update on patient service backend code

---
+++ src/main/java/dao/impl/PatientDaoImpl.java
@@ -0,0 +1,33 @@
+package dao.impl;
+
+import dao.PatientDao;
+import jakarta.enterprise.context.ApplicationScoped;
+import models.Patient;
+import org.hibernate.Hibernate;
+import java.util.Optional;
+
+
+public class PatientDaoImpl extends AbstractDao<Patient, Long> implements PatientDao {
+
+    public PatientDaoImpl() {
+        super(Patient.class);
+    }
+
+    @Override
+    public Optional<Patient> findByIdWithHistory(Long id) {
+        return execute(session -> {
+            Optional<Patient> patientOpt = Optional.ofNullable(session.get(Patient.class, id));
+            patientOpt.ifPresent(p -> Hibernate.initialize(p.getMedicalHistory()));
+            return patientOpt;
+        });
+    }
+
+    @Override
+    public void delete(Patient entity) {
+        execute(session -> {
+            entity.setDeleted(true);
+            session.merge(entity);
+            return null;
+        });
+    }
+}

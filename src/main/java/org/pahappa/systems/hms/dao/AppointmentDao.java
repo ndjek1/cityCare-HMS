efb0c8ee38984a4e34c261d6a4947bd313ae085b
ndjek1 <vndjekornom@gmail.com>
2025-07-14 16:14:25 +0300

    Moved crud operations of the appointment service to the doa and reorganized the appointment service

--- src/main/java/org/pahappa/systems/hms/dao/AppointmentDao.java
+++ src/main/java/org/pahappa/systems/hms/dao/AppointmentDao.java
@@ -4,12 +4,18 @@ import org.pahappa.systems.hms.constants.AppointmentStatus;
 import org.pahappa.systems.hms.models.Appointment;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentDao extends GenericDao<Appointment, Long> {
-    public List<Appointment> findByDoctorId(Long doctorId);
-    public List<Appointment> findByPatientId(Long patientId);
+
     public List<Appointment> findByDateAndStatus(LocalDate date, AppointmentStatus status);
     public List<Appointment> findByStatus( AppointmentStatus status);
-
+    public List<Appointment> getAppointmentsForDoctor(Long doctorId);
+    boolean recordDiagnosis(Long appointmentId, String diagnosisNotes);
+    boolean rescheduleAppointment(Long appointmentId, Long newDoctorId, LocalDateTime newDateTime);
+    boolean cancelAppointment(Long appointmentId);
+    Optional<Appointment> findAppointmentById(Long appointmentId);
+    List<Appointment> getAppointmentsForPatient(Long patientId);
 }
